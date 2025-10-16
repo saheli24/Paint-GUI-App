f5d0e027036650056bf7ea88558470f33e49fa31
Saheli <sahelisaha47@gmail.com>
2025-10-15 22:31:16 -0400

    Highlight Current Buttons

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/ShapeChooserPanel.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/ShapeChooserPanel.java
@@ -3,10 +3,15 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+
+import java.util.ArrayList;
+import java.util.List;
 //Need to figure out how to highlight buttons for bug
 public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEvent> {
 
         private View view;
+        private List<Button> buttons = new ArrayList<>(); // store buttons
+        private Button selectedButton = null; // track button currently selected
 
         public ShapeChooserPanel(View view) {
 
@@ -26,10 +31,28 @@ public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEv
 
         @Override
         public void handle(ActionEvent event) {
-                String command = ((Button) event.getSource()).getText();
-                view.setMode(command);
-                System.out.println(command);
+            Button clickedButton = (Button) event.getSource();
+            String command = clickedButton.getText();
+            view.setMode(command);
+            System.out.println(command);
+
+            if (selectedButton != null) {
+                selectedButton.setStyle(""); // clears the style
+            }
+
+            clickedButton.setStyle(
+                    "-fx-background-color: grey; " +            // button fill
+                            "-fx-font-weight: bold; " +         // bold text
+                            "-fx-text-fill: white; " +          // text color
+                            "-fx-border-color: black; " +       // black outline
+                            "-fx-border-width: 2px; " +         // border thickness
+                            "-fx-border-radius: 5px; " +        // rounded corners
+                            "-fx-background-radius: 5px;"       // background match border radius
+            );
+
+            selectedButton = clickedButton;
         }
+
 }
 
 
