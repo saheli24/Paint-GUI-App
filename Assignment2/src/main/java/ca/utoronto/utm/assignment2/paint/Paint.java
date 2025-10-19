a064d88670c2ba8e80700940f9a9c4906c8c7001
Saheli <sahelisaha47@gmail.com>
2025-10-18 20:28:05 -0400

    Figuring out what classes I need to implement or use to complete it

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/Paint.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/Paint.java
@@ -6,6 +6,7 @@ import javafx.stage.Stage;
 // 1) While the mouse is being dragged, dynamically draw a temporary rectangle from the initial click point to the current mouse position (mid-construction view).
 // 2) On mouse release, finalize the rectangle by adding it to the canvas or shapes list.
 // Required to implement a Rectangle class in order to achieve US1.004
+// Classes I need to implement/use in order to complete: Rectangle Class, Paint Model, Paint Panel, Point
 public class Paint extends Application {
 
         PaintModel model; // Model
