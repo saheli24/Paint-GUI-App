eb6a27bbd25dd3a0a8314f19ebd09409e59a9eac
Saheli <sahelisaha47@gmail.com>
2025-10-19 22:01:48 -0400

    Figuring out what US needs to be completed in order to implement it

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/Paint.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/Paint.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 // 2) On mouse release, finalize the rectangle by adding it to the canvas or shapes list.
 // Required to implement a Rectangle class in order to achieve US1.004
 // Classes I need to implement/use in order to complete: Rectangle Class, Paint Model, Paint Panel, Point
+// Bugs/US that need to be complete in order to implement the result: US1.003 which is creating the drawing rectangle class
 public class Paint extends Application {
 
         PaintModel model; // Model
