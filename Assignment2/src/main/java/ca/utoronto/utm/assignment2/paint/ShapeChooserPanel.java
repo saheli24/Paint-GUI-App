1d8626ecef5005f51bdf8c06c131b3a86585af80
Anas H. <anas.habib@mail.utoronto.ca>
2025-10-22 01:45:14 -0400

    Completed BUG1.001 | habiban4

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/ShapeChooserPanel.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/ShapeChooserPanel.java
@@ -3,6 +3,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 
 //Need to figure out how to highlight buttons for bug
 public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEvent> {
@@ -12,24 +15,89 @@ public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEv
 
         public ShapeChooserPanel(View view) {
 
-                this.view = view;
+            this.view = view;
 
-                String[] buttonLabels = { "Circle", "Rectangle", "Square", "Squiggle", "Polyline" };
+            // creates image for circle
+            Image iconOne = new Image(getClass().getResourceAsStream("/icons/circle.png"));
+            ImageView iconViewOne = new ImageView(iconOne);
+            iconViewOne.setFitWidth(15);
+            iconViewOne.setFitHeight(15);
 
-                int row = 0;
-                for (String label : buttonLabels) {
-                        Button button = new Button(label);
-                        button.setMinWidth(100);
-                        this.add(button, 0, row);
-                        row++;
-                        button.setOnAction(this);
-                }
+            // sets image for circle
+            Button circleButton = new Button();
+            circleButton.setUserData("Circle");
+            circleButton.setGraphic(iconViewOne);
+            circleButton.setText("");
+            circleButton.setMinWidth(100);
+            this.add(circleButton, 0, 0);
+            circleButton.setOnAction(this);
+
+            // creates image for rectangle
+            Image iconTwo = new Image(getClass().getResourceAsStream("/icons/rectangle.png"));
+            ImageView iconViewTwo = new ImageView(iconTwo);
+            iconViewTwo.setFitWidth(15);
+            iconViewTwo.setFitHeight(15);
+
+            // sets image for rectangle
+            Button rectangleButton = new Button();
+            rectangleButton.setUserData("Rectangle");
+            rectangleButton.setGraphic(iconViewTwo);
+            rectangleButton.setText("");
+            rectangleButton.setMinWidth(100);
+            this.add(rectangleButton, 0, 1);
+            rectangleButton.setOnAction(this);
+
+            // sets image for square
+            Image iconThree = new Image(getClass().getResourceAsStream("/icons/square.png"));
+            ImageView iconViewThree = new ImageView(iconThree);
+            iconViewThree.setFitWidth(15);
+            iconViewThree.setFitHeight(15);
+
+            // sets image for square
+            Button squareButton = new Button();
+            squareButton.setUserData("Square");
+            squareButton.setGraphic(iconViewThree);
+            squareButton.setText("");          // hide text
+            squareButton.setMinWidth(100);
+            this.add(squareButton, 0, 2);
+            squareButton.setOnAction(this);
+
+            // sets image for squiggle
+            Image iconFour = new Image(getClass().getResourceAsStream("/icons/squiggle.png"));
+            ImageView iconViewFour = new ImageView(iconFour);
+            iconViewFour.setFitWidth(15);
+            iconViewFour.setFitHeight(15);
+
+            // sets image for squiggle
+            Button squiggleButton = new Button();
+            squiggleButton.setUserData("Squiggle");
+            squiggleButton.setGraphic(iconViewFour);
+            squiggleButton.setText("");
+            squiggleButton.setMinWidth(100);
+            this.add(squiggleButton, 0, 3);
+            squiggleButton.setOnAction(this);
+
+            // sets image for polyline
+            Image iconFive = new Image(getClass().getResourceAsStream("/icons/polyline.png"));
+            ImageView iconViewFive = new ImageView(iconFive);
+            iconViewFive.setFitWidth(15);
+            iconViewFive.setFitHeight(15);
+
+            // sets image for polyline
+            Button polylineButton = new Button();
+            polylineButton.setUserData("Polyline");
+            polylineButton.setGraphic(iconViewFive);
+            polylineButton.setText("");        // hide text
+            polylineButton.setMinWidth(100);
+            this.add(polylineButton, 0, 4);
+            polylineButton.setOnAction(this);
+
         }
 
         @Override
         public void handle(ActionEvent event) {
             Button clickedButton = (Button) event.getSource();
-            String command = clickedButton.getText();
+            String command = (String) clickedButton.getUserData(); // gets data of button instead of label
             view.setMode(command);
             System.out.println(command);
 
